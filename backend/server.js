1fa3a8f2ebf0dc939658a071fe104b1a6c0db814
shaikh-mohd-samir <mohdsameershaikh2003@gmail.com>
2026-04-21 14:44:50 +0530

    final cors fix + route fix

--- backend/server.js
+++ backend/server.js
@@ -19,17 +19,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like Postman)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: "*",
 }));
 
 // ✅ MIDDLEWARE
